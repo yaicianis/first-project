54593059fad6cccaf6aa6fc3d24be67ac9c5daf1
yaicianis <115881560+yaicianis@users.noreply.github.com>
2024-06-16 16:20:47 +0100

    f1

---
+++ src/app/first/FirstComponent.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-first',
+  templateUrl: './first.component.html',
+  styleUrls: ['./first.component.css']
+})
+export class FirstComponent implements OnInit {
+
+hero ={
+  name:'',
+  power: 0,
+  image:'',
+}
+heros :any[]=[];
+ajout(){
+  this.heros.push(this.hero);
+  this.hero={
+    name:'',
+    power: 0,
+    image:'',
+  }
+  console.log(this.heros);
+}
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/first/first.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FirstComponent } from './FirstComponent';
+
+describe('FirstComponent', () => {
+  let component: FirstComponent;
+  let fixture: ComponentFixture<FirstComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FirstComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FirstComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
